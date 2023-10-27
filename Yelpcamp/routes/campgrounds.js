0139ea79f473fa4e23a5bca190acadaeecb769f9
Vishnu Koundinya Challa <c.vishnuk37@gmail.com>
2023-10-27 16:14:57 -0500

    Yelpcamp/views/partials/navbar.ejs

--- Yelpcamp/routes/campgrounds.js
+++ Yelpcamp/routes/campgrounds.js
@@ -2,25 +2,9 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../errors/catchAsync');
 const Campground = require('../models/campground');
-const {campgroundSchema}=require('../schemas');
-const ObjectID = require('mongoose').Types.ObjectId;
-const ExpressError = require('../errors/ExpressError');
-const {isLoggedIn} = require('../isLoggedIn');
-const campground = require('../models/campground');
 
+const {isLoggedIn,validateCampground,isAuthor} = require('../isLoggedIn');
 
-
-
-const validateCampground = (req,res,next)=>{
-    
-    const {error} = campgroundSchema.validate(req.body)
-    if(error){
-        const msg = error.details.map(el=>el.message).join('')
-        throw new ExpressError(msg,400)
-    }else{
-        next();
-    }
-}
 
 router.get('/',catchAsync( async (req,res)=>{
     const campgrounds = await Campground.find({});
@@ -49,21 +33,17 @@ router.get('/:id',isLoggedIn ,catchAsync (async(req,res,next)=>{
     }
     res.render('campgrounds/show',{campground})
 }))
-router.get('/:id/edit',isLoggedIn, catchAsync( async(req,res)=>{
+router.get('/:id/edit',isLoggedIn,isAuthor, catchAsync( async(req,res)=>{
     const campground = await Campground.findById(req.params.id);
      res.render('campgrounds/edit',{campground})
 }))
-router.put('/:id',isLoggedIn, validateCampground, catchAsync ( async(req,res)=>{
+router.put('/:id',isLoggedIn, isAuthor,validateCampground, catchAsync ( async(req,res)=>{
     const {id}=req.params;
-    const campground = await Campground.findById(id);
-    if (!(campground.author.equals(req.user._id))){
-        req.flash('You do not have permission to do that')
-    }
-    //const camp = await Campground.findByIdAndUpdate(id,{...req.body.campground})
+    const camp = await Campground.findByIdAndUpdate(id,{...req.body.campground})
     req.flash('success','Sucessfully updated Campground')
     res.redirect(`/campgrounds/${camp._id}`)
 }))
-router.delete('/:id',isLoggedIn, catchAsync (async (req,res)=>{
+router.delete('/:id',isLoggedIn,isAuthor, catchAsync (async (req,res)=>{
     const {id}=req.params;
     await Campground.findByIdAndDelete(id);
     req.flash('del','Sucessfully deleted Campground')
--- Yelpcamp/routes/reviews.js
+++ Yelpcamp/routes/reviews.js
@@ -1,21 +1,11 @@
 const express = require('express');
 const router = express.Router({mergeParams:true});
-
 const catchAsync = require('../errors/catchAsync');
 const Campground = require('../models/campground');
 const Review = require('../models/review');
-const {reviewSchema}=require('../schemas');
-const ExpressError = require('../errors/ExpressError');
+const {validateReview}=require('../isLoggedIn')
 
-const validateReview = (req,res,next)=>{
-    const {error} = reviewSchema.validate(req.body)
-    if(error){
-        const msg = error.details.map(el=>el.message).join('')
-        throw new ExpressError(msg,400)
-    }else{
-        next();
-    }
-}
+
 
 router.post('/', validateReview, catchAsync(async (req,res)=>{
     const campground = await Campground.findById(req.params.id)
